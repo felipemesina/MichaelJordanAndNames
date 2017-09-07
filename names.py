cd2b592a0b086c6dc2f0873b89aa55fa3adbc2e0
Felipe <felipejmesina@gmail.com>
2017-09-06 19:45:02 -0700

    completed assignment

---
+++ names.py
@@ -0,0 +1,39 @@
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+def studentNames():
+    for i in range(len(students)):
+        print students[i]["first_name"] + " " + students[i]["last_name"]
+
+studentNames()
+
+
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+def moreStudents(users):
+    for i in users:
+        counter = 0
+        print i
+        for person in users[i]:
+            counter += 1
+            first_name = person['first_name'].upper()
+            last_name = person['last_name'].upper()
+            length = len(first_name) + len(last_name)
+            print "{} - {} {} - {}".format(counter, first_name, last_name, length)
+
+moreStudents(users)
